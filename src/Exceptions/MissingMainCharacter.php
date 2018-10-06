25e7c7c567ceb3427d456a7e7fb0cfce863741ec
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2018-10-06 11:15:45 +0200

    fix(job): Fix logout job construct in order to avoid serializing issues
    Jobs should never do other things than entity properties instanciation during construct stage.
    Otherwise, Laravel will not be able to serialize it properly, resulting in a closure exception.
    everything else than __construct will be done once the job is running. Whereas __construct is call
    when the job is put in queue.

---
+++ src/Exceptions/MissingMainCharacter.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Herpaderpaldent\Seat\SeatDiscourse\Exceptions;
+
+
+use Exception;
+use Seat\Web\Models\Group;
+
+class MissingMainCharacterException extends Exception
+{
+    /**
+     * MissingMainCharacterException constructor.
+     * @param \Seat\Web\Models\Group $group
+     */
+    public function __construct(Group $group)
+    {
+        $message = sprintf('The group with ID %d does not have a main character set, ' .
+            'or related character information is missing.', $group->id);
+        parent::__construct($message, 0, null);
+    }
+}
